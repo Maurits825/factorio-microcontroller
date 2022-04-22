12170f96248f9ce98ea92311b59ec3575712196e
Maurits <mauritsvdsteur@live.com>
2022-04-22 19:02:38 +0200

    fix test resource path

--- tests/factorio_compiler_test.py
+++ tests/factorio_compiler_test.py
@@ -2,6 +2,9 @@ from factorio_microcontroller.factorio_compiler import FactorioCompiler
 import unittest
 import filecmp
 import os
+from pathlib import Path
+
+TEST_RESOURCE_FOLDER = Path(__file__).parent.parent / "tests/resources"
 
 
 class FactorioCompilerTest(unittest.TestCase):
@@ -9,10 +12,11 @@ class FactorioCompilerTest(unittest.TestCase):
         self.fc = FactorioCompiler()
 
     def test_compile_to_bin(self):
-        assembly_file = "./resources/test_assembly.txt"
-        actual_assembly_file = "./resources/test_bin_actual.txt"
+        assembly_file = TEST_RESOURCE_FOLDER / "test_assembly.txt"
+        actual_assembly_file = TEST_RESOURCE_FOLDER / "test_bin_actual.txt"
         self.fc.compile_to_bin(assembly_file, actual_assembly_file)
-        self.assertTrue(filecmp.cmp("./resources/test_bin_expected.txt", "./resources/test_bin_actual.txt"))
+        self.assertTrue(filecmp.cmp(TEST_RESOURCE_FOLDER / "test_bin_expected.txt",
+                                    TEST_RESOURCE_FOLDER / "test_bin_actual.txt"))
 
         os.remove(actual_assembly_file)
 
