64762c519172af02a7961fccae099f02a80fd779
Maurits <63048736+Maurits825@users.noreply.github.com>
2022-05-13 15:57:11 +0200

    fixed test

--- tests/factorio_microcontroller_simulator_test.py
+++ tests/factorio_microcontroller_simulator_test.py
@@ -1,7 +1,8 @@
-import unittest
+import sys
 from pathlib import Path
-
-from factorio_microcontroller_simulator.factorio_microcontroller_sim import FactorioMicrocontrollerSim
+import unittest
+sys.path.append(str(Path(__file__).parent.parent / "factorio_microcontroller_simulator"))
+from factorio_microcontroller_simulator import factorio_microcontroller_sim
 
 TEST_RESOURCE_FOLDER = Path(__file__).parent.parent / "tests/resources"
 
@@ -9,7 +10,7 @@ TEST_RESOURCE_FOLDER = Path(__file__).parent.parent / "tests/resources"
 class FactorioMicrocontrollerSimTest(unittest.TestCase):
     def setUp(self):
         fib_binary_file = TEST_RESOURCE_FOLDER / "fibonacci_binary.txt"
-        self.simulator = FactorioMicrocontrollerSim(fib_binary_file)
+        self.simulator = factorio_microcontroller_sim.FactorioMicrocontrollerSim(fib_binary_file)
 
     def test_simulate_fibonacci(self):
         state = self.simulator.simulate()
